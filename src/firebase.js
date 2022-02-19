593f11f691bb2bfa2b8b30d7f1edccbbce0d770c
williams <oneal.wills@gmail.com>
2022-02-19 13:52:05 +0100

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAAwnt2dVuqNJrbMup2K72CpW2_X8pixFM",
+//   authDomain: "whatspp-mern-aec5d.firebaseapp.com",
+//   projectId: "whatspp-mern-aec5d",
+//   storageBucket: "whatspp-mern-aec5d.appspot.com",
+//   messagingSenderId: "228031052147",
+//   appId: "1:228031052147:web:f947ca0602178d599bed06",
+// };
