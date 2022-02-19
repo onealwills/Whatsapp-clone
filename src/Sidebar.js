03282e0f2205abb90e1c7e82a798aeab773431fd
williams <oneal.wills@gmail.com>
2022-02-19 20:21:52 +0100

    update

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,40 @@
 import React from "react";
 import "./Sidebar.css";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SearchOutLined from "@material-ui/icons/SearchOutlined";
+import { Avatar, IconButton } from "@material-ui/core";
+import SidebarChat from "./SidebarChat";
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <h1>Sidebar</h1>
+      <div className="sidebar__header">
+        <Avatar src="https://www.ap.be/sites/default/files/2020-07/alvanjongsafaan.jpg" />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchOutLined />
+          <input placeholder="Search or start new chat" type="text" />
+        </div>
+      </div>
+      <div className="sidebar__chats">
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+      </div>
     </div>
   );
 }
